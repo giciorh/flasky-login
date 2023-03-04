e578e2222909d4de329cb4909985c77ec5d2379b
giciorh <126015418+giciorh@users.noreply.github.com>
2023-03-04 20:08:02 +0000

    added simple check form field and flash msg in in auth.py and in base.html mechanism to  show proper msg accordingly to category of message

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['post', 'get'])
 def login():
+  data = request.form
+  print(data)
   return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
 def logout():
-  return '<h2>Logout</h2>'
+  return '<h2>Logged out</h2>'
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['POST', 'GET'])
 def signup():
+  if request.method == 'POST':
+    email = request.form['email']
+    firstName = request.form['firstName']
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if len(email) < 4:
+      flash('Email must be greater than 4 chars.', category="error")
+    elif len(firstName) < 2:
+      flash('Name must be greater than 2 chars.', category="error")
+    elif password1 != password2:
+      flash('Passwords don\'t match.', category="error")
+    elif len(password1) < 8:
+      flash('Password must be greater than 7 chars.', category="error")
+    else:
+      #add user to db
+      flash('Account was created.', category="success")
+
   return render_template("signup.html")
